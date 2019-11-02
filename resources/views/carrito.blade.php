1eb4214897bed77adbe8d36cfa16d4ce091c1088
unknown <samsha1901@icloud.com>
2019-11-02 10:32:07 -0300

    total-cart

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -27,48 +27,60 @@
             <thead>
                 <tr>
                     <th class="text-center">Referencia</th>
-                    <th style="width: 200px;" >Precio</th>
+                    <th>Producto</th>
+                    <th style="width: 100px;" >Precio</th>
                     <th style="width: 50px;">+</th>
-                    <th>Subtotal</th>
-                    <th class="">Acciones</th>
+                    <th class="text-right" style="width: 100px;">Subtotal</th>
+                    <th style="width: 80px;">Acciones</th>
                 </tr>
             </thead> 
             <tbody>
           @foreach (auth()->user()->cart->details as $detail)
               <tr>
                 <td class="text-center"> <img width="50px" height="50px" src="{{asset('imagenes/productos/'.$detail->producto->imagen)}}" alt="Circle Image" class="img-raised img-thumbnail img-fluid">
-                  <p>{{ $detail->producto->nombre }}</p></td>
+                </td>
+                <td>{{ $detail->producto->nombre }}</td>
                 <td>$ {{ $detail->producto->precio }}</td>
                 <td>{{ $detail->cantidad }}</td>
-                <td>$ {{ $detail->cantidad * $detail->producto->precio }}</td>
-                <td class="td-actions ">
+                <td class="text-right">$ {{ $detail->cantidad * $detail->producto->precio }}</td>
+                <td>
                   <form method="post" action="{{ url('/cart') }}">
                     {{ csrf_field() }}
                     {{ method_field('DELETE') }}
                     <input type="hidden" name="cart_detail_id" value="{{ $detail->id }}">
-                    <a href="{{ url('/products/'.$detail->producto->idproducto) }}" target="_blank" rel="tooltip" title="Ver detalle" class="btn btn-info btn-simple btn-sm">
+                    <!-- <a href="{{ url('/products/'.$detail->producto->idproducto) }}" target="_blank" rel="tooltip" title="Ver detalle" class="btn btn-info btn-simple btn-sm">
                     <i class="fa fa-info"></i>
-                  </a>
+                  </a> -->
                   <button type="submit" rel="tooltip" title="Remover" class="btn btn-danger btn-simple btn-sm">
                     <i class="fa fa-times"></i>
                   </button>
                   </form>
-                  
                 </td>
               </tr>
           @endforeach
             </tbody>
+            <tfoot>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="text-right"><strong>TOTAL</strong></td>
+                <td class="text-right"><strong>$ {{ auth()->user()->cart->total }}</strong></td>
+                <td></td>
+              </tr>
+              
+            </tfoot>
         </table>
+        <div class="text-center">
           <div class="form-group">
             <form method="post" action="{{ url('/order') }}">
               {{ csrf_field() }}
                 <button type="submit" class="btn btn-info btn-round">
                 <i class="material-icons">done</i>Realizar pedido
                 </button>
-                <button type="reset" class="btn btn-danger btn-round">Volver</button>
             </form>
-              
           </div>
+        </div>
         </div>   
         </div>
       </div>
